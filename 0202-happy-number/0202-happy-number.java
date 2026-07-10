b7eb6508891c739e155e8d75b6d0b87559ee30a0
Anshu Raj <rajanshu4021@gmail.com>
2026-07-11 00:07:25 +0530

    Time: 1 ms (78.27%), Space: 42.2 MB (89.28%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -11,18 +11,11 @@ class Solution {
             fast = square(square(fast));
         } while (slow != fast);
 
-//if a cycle exists, then the number is not a happy number
-//and slow will have a value other than 1
-
         return slow == 1;
     }
-    
-//Finding the square of the digits of a number
 
     public int square(int num) {
-        
-        int ans = 0;
-        
+         int ans = 0;
         while(num > 0) {
             int remainder = num % 10;
             ans += remainder * remainder;
