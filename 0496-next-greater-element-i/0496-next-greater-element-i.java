3c9bbdfada51d79915cafc6157094f885a36d21a
Anshu Raj <rajanshu4021@gmail.com>
2026-07-20 03:32:03 +0530

    Time: 4 ms (74.46%), Space: 45.4 MB (22.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -20,9 +20,9 @@ for (int i = nums2.length - 1; i >= 0; i--) {
 int[] ans = new int[nums1.length];
 
 for (int i = 0; i < nums1.length; i++) {
-    ans[i] = map.get(nums1[i]);
+    nums1[i] = map.get(nums1[i]);
 }
 
-return ans;
+return nums1;
     }
 }
